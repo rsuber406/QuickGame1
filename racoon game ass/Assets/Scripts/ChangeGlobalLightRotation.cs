161631d64418b82432deab3563ff356bdc621044
Cyrus2234 <HLsuderman@student.fullsail.edu>
2025-02-28 14:28:57 -0500

    Started Global Light Rotation

---
+++ racoon game ass/Assets/Scripts/ChangeGlobalLightRotation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ChangeGlobalLightRotation : MonoBehaviour
+{
+    [SerializeField] GameObject globalDirectionalLight;
+    public float xRotation = 0;
+    public float yRotation = 0;
+    public float zRotation = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
