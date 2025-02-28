c1294f48ea5925ad80f1e94743bb78fd95805821
Cyrus2234 <HLsuderman@student.fullsail.edu>
2025-02-28 14:36:20 -0500

    made change

--- racoon game ass/Assets/Scripts/ChangeGlobalLightRotation.cs
+++ racoon game ass/Assets/Scripts/ChangeGlobalLightRotation.cs
@@ -5,11 +5,11 @@ public class ChangeGlobalLightRotation : MonoBehaviour
     [SerializeField] GameObject globalDirectionalLight;
     public float xRotation = 0;
     public float yRotation = 0;
-    public float zRotation = 0;
+   // public float zRotation = 0;
 
     // Update is called once per frame
     void Update()
     {
-        globalDirectionalLight.transform.rotation = Quaternion.Euler(xRotation, yRotation, zRotation);
+       // globalDirectionalLight.transform.rotation = Quaternion.Euler(xRotation, yRotation, zRotation);
     }
 }
