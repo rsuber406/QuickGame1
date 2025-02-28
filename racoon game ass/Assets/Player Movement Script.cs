4e5f493153a66b22545ec033431116842c7f5e57
imMrMadlad <toroalmodovars@gmail.com>
2025-02-28 14:39:09 -0500

    added basic things

---
+++ racoon game ass/Assets/Player Movement Script.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+
+
+
+
+}
