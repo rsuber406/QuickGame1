e6caad5084871bbf69ac34b28b07ef51db2c8312
DakotaCottman <djcottman@gmail.com>
2025-02-28 17:43:08 -0500

    added ui stuff

--- racoon game ass/Assets/Scripts/ScoreManager.cs
+++ racoon game ass/Assets/Scripts/ScoreManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     private int trashCollected = 0;
+    private int maxTrashInLevel = 16;
     [SerializeField] private TMP_Text trashCountText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void AddTrash()
@@ -19,7 +20,13 @@ public class ScoreManager : MonoBehaviour
 
     private void UpdateUI()
     {
-        trashCountText.text = trashCollected.ToString();
+        trashCollected = 0;
+        trashCountText.text = $"{trashCollected}/{maxTrashInLevel}";
+    }
+
+    public bool ALlTrashCollected()
+    {
+        return trashCollected >= maxTrashInLevel;
     }
 
     public void ResetTrash()
